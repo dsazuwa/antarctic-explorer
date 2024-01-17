1b4d8bc1d472d5255f76814c57e431fab46643a4
dosazuwa <dsazuwa@gmail.com>
2024-01-17 18:45:11 +0100

    style: configure tailwind colors and font

---
+++ src/font.ts
@@ -0,0 +1,11 @@
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+  preload: true,
+});
+
+export default montserrat;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        misty_blue: "#E7ECEF",
+        navy: "#274C77",
+        sky_blue: "#6096BA",
+        pale_azure: "#A3CEF1",
+        stone_gray: "#8B8C89",
+      },
+    },
   },
   plugins: [],
 };
