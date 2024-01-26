a7dd2fe4b607b418b1b1b61fdd7e960308ad6426
dosazuwa <dsazuwa@gmail.com>
2024-01-26 10:41:27 +0100

    change padding and text styles in ClearFilters btn

--- src/components/ClearFilters.tsx
+++ src/components/ClearFilters.tsx
@@ -2,7 +2,7 @@ import { Button } from './ui/button';
 
 function ClearFilters({ disabled }: { disabled: boolean }) {
   return (
-    <Button disabled={disabled} size='xs'>
+    <Button disabled={disabled} size='xs' className='px-3 capitalize'>
       Clear all filters
     </Button>
   );
