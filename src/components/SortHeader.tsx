db6859998be6f771b146f6df65475b8249b3c7b9
dosazuwa <dsazuwa@gmail.com>
2024-02-09 19:10:25 +0100

    use shadcn select in SortHeader

--- src/components/SortHeader.tsx
+++ src/components/SortHeader.tsx
@@ -1,5 +1,12 @@
 import { sortOptions } from '@/lib/constants';
 import { setSortOption, useAppDispatch, useAppSelector } from '@/store';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 function SortHeader() {
   const dispatch = useAppDispatch();
@@ -12,19 +19,26 @@ function SortHeader() {
       <div className='flex flex-row items-center'>
         <div className='mr-1 font-semibold text-slate-500'>Sort</div>
 
-        <select
-          className='select select-xs px-1 py-1'
-          value={selectedSort}
-          onChange={(e) =>
-            dispatch(setSortOption(parseInt(e.target.value, 10)))
-          }
+        <Select
+          onValueChange={(i) => dispatch(setSortOption(parseInt(i, 10)))}
+          value={selectedSort + ''}
+          defaultValue={selectedSort + ''}
         >
-          {sortOptions.map((option, index) => (
-            <option key={`sortOption${index}`} value={index}>
-              {option.displayText}
-            </option>
-          ))}
-        </select>
+          <SelectTrigger
+            id='items per page'
+            className='h-[28px] w-48 px-1 py-1 text-xxs font-semibold'
+          >
+            <SelectValue defaultValue={selectedSort} />
+          </SelectTrigger>
+
+          <SelectContent>
+            {sortOptions.map((option, index) => (
+              <SelectItem key={`sortOption${index}`} value={index + ''}>
+                {option.displayText}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
       </div>
     </div>
   );
