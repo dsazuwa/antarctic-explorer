2f482a598c263e9a72978cff878f54f69faa4ddc
dosazuwa <dsazuwa@gmail.com>
2024-02-23 13:18:48 +0100

    add margin to H3Header component

--- src/components/features/expedition/H3Heading.tsx
+++ src/components/features/expedition/H3Heading.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function H3Heading({ text }: { text: string }) {
-  return <h3 className='text-2xl font-bold text-sky-900'>{text}</h3>;
+  return <h3 className='text-2xl mb-2 font-bold text-sky-900'>{text}</h3>;
 }
