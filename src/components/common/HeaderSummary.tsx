d0e1dedc293514d4e7a4b3409374564431552ac6
dosazuwa <dsazuwa@gmail.com>
2024-02-25 16:40:47 +0100

    add case for when totalItems = 2 to HeaderSummary

--- src/components/common/HeaderSummary.tsx
+++ src/components/common/HeaderSummary.tsx
@@ -18,9 +18,17 @@ export default function HeaderSummary({
     <div className='mr-2 space-x-0.5 text-xxs font-semibold text-slate-500 md:space-x-[3px] md:text-xs'>
       <span>Showing</span>
 
-      {totalItems === 0 ? (
-        <span>0</span>
-      ) : (
+      {totalItems === 0 && <span>0</span>}
+
+      {totalItems === 1 && (
+        <>
+          <span className='font-bold text-black'>{start + 1}</span>
+          <span>of</span>
+          <span className='font-bold text-black'>{totalItems}</span>
+        </>
+      )}
+
+      {totalItems > 1 && (
         <>
           <span className='font-bold text-black'>{start + 1}</span>
           <span>-</span>
