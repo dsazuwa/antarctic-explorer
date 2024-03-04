215452c12778901a1ab920ba00364c84f8f8634c
dosazuwa <dsazuwa@gmail.com>
2024-03-04 16:39:47 +0100

    create 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,14 +2,9 @@ import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 
-type Props = {
-  href: string;
-  buttonLabel: string;
-};
-
-export default function PageNotFound({ href, buttonLabel }: Props) {
+export default function PageNotFound() {
   return (
-    <div className='mt-[50px] w-full px-4'>
+    <div className='mt-[15%] w-full px-4'>
       <div className='mx-auto flex w-full max-w-screen-lg flex-col items-center space-y-2 text-center'>
         <div className='text-6xl font-bold text-navy sm:text-7xl md:text-8xl lg:text-9xl'>
           404
@@ -21,9 +16,9 @@ export default function PageNotFound({ href, buttonLabel }: Props) {
           Sorry we can&apos;t find the page you&apos;re looking for.
         </div>
 
-        <Link href={href}>
+        <Link href='/'>
           <Button className='bg-navy capitalize hover:bg-navy/90'>
-            {buttonLabel}
+            Back to Expeditions
           </Button>
         </Link>
       </div>
