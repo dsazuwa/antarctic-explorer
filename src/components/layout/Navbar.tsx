5755a0548a3f9d708765680c633c099ffa01b991
dosazuwa <dsazuwa@gmail.com>
2024-03-17 15:16:06 +0100

    femove fixed positioning from navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,17 +4,13 @@ import Logo from '../Logo';
 
 function Navbar() {
   return (
-    <>
-      <nav className='fixed top-0 z-10 h-14 w-full bg-primary-foreground'>
-        <div className='ml-auto mr-auto flex w-full max-w-screen-lg flex-row px-4 py-2'>
-          <Link href='/' className='flex-grow'>
-            <Logo size={'sm'} />
-          </Link>
-        </div>
-      </nav>
-
-      <div className='mt-14' />
-    </>
+    <nav className='h-14 w-full bg-primary-foreground'>
+      <div className='ml-auto mr-auto flex w-full max-w-screen-lg flex-row px-4 py-2'>
+        <Link href='/' className='flex-grow'>
+          <Logo size={'sm'} />
+        </Link>
+      </div>
+    </nav>
   );
 }
 
