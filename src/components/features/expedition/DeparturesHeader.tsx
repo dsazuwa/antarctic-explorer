044fe24dacc33384b667bf5bfd69eacf4b3f063d
dosazuwa <dsazuwa@gmail.com>
2024-04-22 14:03:38 +0100

    fix error in state and end item calculation

--- src/components/features/expedition/DeparturesHeader.tsx
+++ src/components/features/expedition/DeparturesHeader.tsx
@@ -1,18 +1,22 @@
 import HeaderSummary from '@/components/common/HeaderSummary';
 import SortSelect from '@/components/common/SortSelect';
-import { departureSortOptions } from '@/lib/constants';
+import {
+  departureSortOptions,
+  departuresPerPageOptions,
+} from '@/lib/constants';
 import { useDeparturesStore } from '@/store/departures';
 
 export default function DeparturesHeader() {
   const { currentPage, totalItems, selectedSort, selectedSize, setSort } =
     useDeparturesStore();
 
+  console.log({ currentPage, totalItems, selectedSort, selectedSize });
   return (
     <div className='inline-flex w-full flex-wrap items-center justify-between'>
       <HeaderSummary
         itemType='departures'
         currentPage={currentPage}
-        itemsPerPage={selectedSize}
+        itemsPerPage={departuresPerPageOptions[selectedSize]}
         totalItems={totalItems}
       />
 
