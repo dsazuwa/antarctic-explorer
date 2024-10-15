91207ac2e3b6c143a8f88369c92b761b41d3a614
dosazuwa <dsazuwa@gmail.com>
2024-10-15 19:20:51 +0100

    Revert "switch to client side rendering when retrieving expeditions"
    This reverts commit 31f2f134c408faf5e09b4ac7c1127432bb6110b9.

---
+++ src/components/expeditions/index.ts
@@ -0,0 +1,17 @@
+import ClearFilters from './clear-filters';
+import ExpeditionsGrid from './expeditions-grid';
+import FilterChips from './filter-chips';
+import FilterPanel from './filter-panel';
+import MobileFilterPanel from './filter-panel-mobile';
+import PaginationControls from './pagination-controls';
+import PaginationHeader from './pagination-header';
+
+export {
+  ClearFilters,
+  ExpeditionsGrid,
+  FilterChips,
+  FilterPanel,
+  MobileFilterPanel,
+  PaginationControls,
+  PaginationHeader,
+};
